5d91c44446fde6ee961e53d86a495c7c41e21b7d
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-08-21 11:07:57 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/monkins1010/ccminer/archive/refs/tags/v3.7.0.tar.gz
-tar xvzf v3.7.0.tar.gz
-cd ccminer-3.7.0
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04.tar.gz
+tar xvzf ccminer_3_7_ubuntu_18.04.tar.gz
+cd ccminer_3_7_ubuntu_18.04
 ./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u 86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeT86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeT -k --tls --rig-id GC-US
