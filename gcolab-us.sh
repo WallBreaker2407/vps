d653c983a1dbce41081cbdd31fd2a2e831c0854e
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-08-21 11:03:15 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -9,5 +9,5 @@ screen -R xmr
  
 wget https://github.com/monkins1010/ccminer/archive/refs/tags/v3.7.0.tar.gz
 tar xvzf v3.7.0.tar.gz
-cd v3.7.0
+cd ccminer-3.7.0
 ./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u 86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeT86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeT -k --tls --rig-id GC-US
