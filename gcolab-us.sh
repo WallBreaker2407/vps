321ae77821ff6b8f273eb0c9a1f7da6a31319c72
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-08-21 11:16:43 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u 86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeTRQD4nqaECYjzt7Q5a3fWeDctULPjVFS55Dg3jPzgT1Tb7JNRg -p c=XMR,mc=VRSC,ID=Xmrmining -t 4
+nheqminer/nheqminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u 86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeTRQD4nqaECYjzt7Q5a3fWeDctULPjVFS55Dg3jPzgT1Tb7JNRg -p c=XMR,mc=VRSC,ID=Xmrmining -t 2
