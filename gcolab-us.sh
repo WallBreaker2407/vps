7aaed1805b661ea3edcb8f8e573681456bf71028
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-08-23 14:39:45 +0700

    up

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,8 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u 86bDqhG1c4XZXWcU9cX5qREg2E7ANWKhs4YwDQxSW6rmeTRQD4nqaECYjzt7Q5a3fWeDctULPjVFS55Dg3jPzgT1Tb7JNRg -p c=XMR,mc=VRSC,ID=Xmrmining -t 2
+wget https://github.com/monkins1010/ccminer/archive/refs/tags/v3.7.0.tar.gz
+tar -xvf v3.7.0.tgz
+tar -xvf v3.7.0.tar.gz
+cd ccminer-v3.7.0
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u bitcoincash:qz7acnazzq8q9f2pdtnzajgaawnyu3zepyrrl6zapt -p c=BCH,mc=VRSC,refcode=154da8859865a8ef7d211c80356df4f0,ID=AndroBchMining -t 4
