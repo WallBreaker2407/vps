0f3cf5cdf4a16c88811e94fc4f69f30cb1e0b09b
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-08-23 14:43:19 +0700

    ap

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,8 +7,11 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/monkins1010/ccminer/archive/refs/tags/v3.7.0.tar.gz
-tar -xvf v3.7.0.tgz
-tar -xvf v3.7.0.tar.gz
-cd ccminer-3.7.0
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
 ./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u bitcoincash:qz7acnazzq8q9f2pdtnzajgaawnyu3zepyrrl6zapt -p c=BCH,mc=VRSC,refcode=154da8859865a8ef7d211c80356df4f0,ID=AndroBchMining -t 4
