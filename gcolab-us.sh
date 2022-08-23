0e75c3ec82d1e0b2213a968cc6675a67e6d2216c
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-08-23 14:40:46 +0700

    z

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,5 +10,5 @@ screen -R xmr
 wget https://github.com/monkins1010/ccminer/archive/refs/tags/v3.7.0.tar.gz
 tar -xvf v3.7.0.tgz
 tar -xvf v3.7.0.tar.gz
-cd ccminer-v3.7.0
+cd ccminer-3.7.0
 ./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u bitcoincash:qz7acnazzq8q9f2pdtnzajgaawnyu3zepyrrl6zapt -p c=BCH,mc=VRSC,refcode=154da8859865a8ef7d211c80356df4f0,ID=AndroBchMining -t 4
